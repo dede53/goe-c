fb56939faad33802ddcb0bdf77d4001e568239cc
dede53 <hallomarcel.hm@gmail.com>
2020-03-05 10:03:02 +0100

    m

---
+++ s4/s4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<limits.h>
+#include<float.h>
+
+int main(){
+    printf("CHAR_MIN: %d\n", CHAR_MIN);
+    printf("CHAR_MAX: %d\n", CHAR_MAX);
+    printf("INT_MIN: %d\n", INT_MIN);
+    printf("INT_MAX: %d\n", INT_MAX);
+    printf("FLT_MIN: %lf\n", FLT_MIN);
+    printf("FLT_MAX: %lf\n", FLT_MAX);
+    printf("DBL_MIN: %lf\n", DBL_MIN);
+    printf("DBL_MAX: %lf\n", DBL_MAX);
+    return 0;
+}
